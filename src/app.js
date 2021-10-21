ea39370a2a5648f2144c995cca4d17170c9e7f57
david-asem <ddy.asem@gmail.com>
2021-10-21 14:48:47 +0000

    added route to serve client from backend server

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const planetsRouter=require('./routes/planets/planetsRouter.js')
 
@@ -10,6 +11,11 @@ app.use(cors({
   origin:'http://localhost:3000'
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
+
 app.use(planetsRouter);
 
 module.exports = app;
