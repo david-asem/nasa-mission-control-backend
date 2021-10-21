cdbdf1573f93eb2e37f3ceaa228c1ae1c3150fa4
david-asem <ddy.asem@gmail.com>
2021-10-21 15:26:01 +0000

    added logging

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 
 const planetsRouter=require('./routes/planets/planetsRouter.js')
 
@@ -10,9 +11,16 @@ const app = express();
 app.use(cors({
   origin:'http://localhost:3000'
 }));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
+//setting default route to index,html
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
+//lauch route
+app.get('/launch', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 })
 
