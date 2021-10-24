4f651eadccb891d24cd4b9b63cef91777e00bd01
david-asem <ddy.asem@gmail.com>
2021-10-24 09:43:30 +0000

    versioned api and moved endpoints to a new file

--- src/app.js
+++ src/app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
-
-const { planetsRouter } = require('./routes/planets/planetsRouter');
-const {launchesRouter} = require('./routes/launches/launchesRouter');
+const api=require('./routes/api')
 
 //JSON parsing middleware
 const app = express();
@@ -16,9 +14,8 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.use('/api/v1/', api);
 
-app.use('/api/v1/planets', planetsRouter);
-app.use('/api/v1/launches', launchesRouter);
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
